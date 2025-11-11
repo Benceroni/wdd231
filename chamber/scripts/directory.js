b8e17a4a829453c697cedff4594711c3080a607f
Benceroni <bell.spencer@icloud.com>
2025-11-10 21:03:09 -0700

    adding logoz

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -13,14 +13,34 @@ getMemberData();
 const displayMembers = (members =>{
     console.table(members);
     members.forEach(member => {
-        const card = document.createElement("section");
-        const _logo = document.createElement("img");
+        const card      = document.createElement("section");
+        const _logo     = document.createElement("img");
+        const _name     = document.createElement("h3");
+        const _phone    = document.createElement("p");
+        const _address  = document.createElement("p");
+        const _website  = document.createElement("a");
         
+        //setting attributes
+
         _logo.setAttribute("src",member.imageAddress);
         _logo.setAttribute("alt",`Image of ${member.name} logo.`);
         _logo.setAttribute("height", "100");
-        card.appendChild(_logo)
 
+        _name.textContent       = member.name;
+        _address.textContent    = member.address;
+        _phone.textContent      = member.phoneNumber;
+        _website.textContent    = member.websiteName;
+        _website.href           = member.websiteURL;
+        
+
+        //card child appends
+        card.appendChild(_logo);
+        card.appendChild(_address);
+        card.appendChild(_phone);
+        card.appendChild(_website);
+        
+
+        //cards card appends
         cards.appendChild(card);
     });
 })
