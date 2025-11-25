1c1c1716410f7655dc1729a29559fc0acd72fa33
Benceroni <bell.spencer@icloud.com>
2025-11-25 14:20:52 -0700

    yeah I'm outa here

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -11,3 +11,16 @@ document.querySelector("#application").innerHTML = `
 <p>Phone number: ${applicationInfo.get("phone")}</p>
 <p>Membership Level: ${applicationInfo.get("level")}</p>
 `
+
+function showmodal(level,price,benefit){
+    const dialog = document.querySelector("#membershipDetails");
+    dialog.innerHTML =`
+    <button id = "closeModal">❌</button>
+    <h2>${level} Membership</h2>
+    <p>$${price} annually</p>
+    <p>Benefits include:<br>${benefit}</p>
+    `
+    dialog.showModal();
+
+    closeModal.addEventListener("click", () => {dialog.close()});
+}
