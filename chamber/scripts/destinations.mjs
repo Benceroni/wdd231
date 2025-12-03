6d7b46eb00e5a5308621fa0e144a0153b3514506
Benceroni <bell.spencer@icloud.com>
2025-12-02 19:27:49 -0700

    lasagnia garf

--- chamber/scripts/destinations.mjs
+++ chamber/scripts/destinations.mjs
@@ -2,6 +2,10 @@ import points_data from "../data/poi.mjs";
 
 const destinations_html = document.querySelector("#destinations");
 
+const flavorText = document.querySelector("#flavorText");
+const lastVisit = localStorage.getItem("lastVisit")
+
+
 // console.table(points_data.points)
 
 destinations_html.innerHTML = ''
@@ -17,3 +21,25 @@ points_data.points.forEach(point => {
     </section>
     `
 });
+
+function flavorTextMessage(){
+    console.log(lastVisit)
+    if (lastVisit){
+        console.log(lastVisit)
+        let days = Math.abs((lastVisit - Date.now())/ 86400000);
+        console.log("Days "+ days)
+        if (days <1){
+            flavorText.innerHTML = "Back so soon! Awesome!";
+        }
+        else{
+            flavorText.innerHTML = `You last visited ${days.toFixed(0)} days ago`;
+        }
+    }
+    else{
+        console.log(new Date)
+        flavorText.innerHTML = "Welcome! Let us know if you have any questions."
+        localStorage.setItem("lastVisit",Date.now())
+    }
+}
+
+flavorTextMessage();
