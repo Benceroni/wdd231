5506263b94df8875512a6ba3779bf2a4e827acaf
Benceroni <bell.spencer@icloud.com>
2025-12-04 22:10:38 -0700

    chang

--- final/scripts/fetch-youtube.mjs
+++ final/scripts/fetch-youtube.mjs
@@ -1,12 +1,16 @@
 const playlistURL = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id=UCUekuiQDM_A2237QIk5_f-w&key=AIzaSyAo-VDju_qyliZ4wK88GuHwHSDmAo5nRkc"
 
+const lastPlaylistFetch = localStorage.getItem("lastFetch");
+const lastPlaylistId = localStorage.getItem("lastId");
+
 async function fetchPlaylist() {
     try{
         const response = await fetch(playlistURL);
         if (response.ok){
             const playlist = await response;
-            console.log(playlist)
-
+            console.log("Response is:")
+            console.log(playlist.json().items[0].contentDetails.relatedPlaylists.uploads)
+            return playlist.json().items[0].contentDetails.relatedPlaylists.uploads;
         }
         else{
             throw Error(await response.text());
@@ -17,6 +21,27 @@ async function fetchPlaylist() {
     }
 }
 
-fetchPlaylist();
+async function fetchYoutube(playlistID) {
+    
+}
+
+function onceDaily(){
+    if (lastPlaylistFetch && lastPlaylistId){
+        let days = Math.abs((lastPlaylistFetch - Date.now())/86400000);
+        if (days >=1){
+            fetchPlaylist();
+            localStorage.setItem("lastFetch",Date.now())
+        }
+        else{
+            console.log("playlist URL fetched in last 24 hours")
+        }
+    }
+    else{
+        localStorage.setItem("lastFetch",Date.now())
+        fetchPlaylist()
+    }
+}
+
+onceDaily()
 
 // export default youtube
